b0fec564680078fbc44fcc512541d3d0979a9287
RomanNihal <nihalroman7@gmail.com>
2024-03-06 13:21:14 +0600

    finished

---
+++ java-group-chat-master/Server/src/function/Method.java
@@ -0,0 +1,41 @@
+package function;
+
+import java.util.ArrayList;
+import javax.swing.JTextArea;
+
+public class Method {
+    
+    private static int clientID;
+    private static ArrayList<Client> clients;
+    private static JTextArea txt;
+
+    public static JTextArea getTxt() {
+        return txt;
+    }
+
+    public static void setTxt(JTextArea aTxt) {
+        txt = aTxt;
+    }
+
+    public static int getClientID() {
+        return clientID;
+    }
+
+    public static void setClientID(int aClientID) {
+        clientID = aClientID;
+    }
+
+    public static ArrayList<Client> getClients() {
+        return clients;
+    }
+
+    public static void setClients(ArrayList<Client> aClients) {
+        clients = aClients;
+        clientID = 1;
+    }
+
+    public static int addClient(Client client) {
+        clients.add(client);
+        return clientID++;
+    }
+}
